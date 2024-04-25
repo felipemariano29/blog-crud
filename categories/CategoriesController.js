37adf21c2cef87285f262c5a71af71db94ee2f2b
Felipe Mariano <86112326+felipemariano29@users.noreply.github.com>
2024-04-25 13:52:36 -0300

    Created Category table with actions (del, edit)

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -15,11 +15,82 @@ router.post("/categories/save", (req, res) => {
       title: title,
       slug: slugify(title, { lower: true }),
     }).then(() => {
-      res.redirect("/");
+      res.redirect("/admin/categories");
     });
   } else {
     res.redirect("/admin/categories/new");
   }
+});
+
+router.get("/admin/categories", (req, res) => {
+  Category.findAll()
+    .then((categories) => {
+      res.render("admin/categories/index", {
+        categories: categories,
+      });
+    })
+    .catch((err) => {
+      res.redirect("/");
+    });
+});
+
+router.post("/categories/delete", (req, res) => {
+  const id = req.body.id;
+
+  if (id !== undefined && !isNaN(id)) {
+    Category.destroy({
+      where: {
+        id: id,
+      },
+    })
+      .then(() => {
+        res.redirect("/admin/categories");
+      })
+      .catch((err) => {
+        console.log("Error while deleting category: " + err);
+        res.redirect("/admin/categories");
+      });
+  } else {
+    res.redirect("/admin/categories");
+  }
+});
+
+router.get("/admin/categories/edit/:id", (req, res) => {
+  const id = req.params.id;
+
+  if (id !== undefined && !isNaN(id)) {
+    Category.findByPk(id)
+      .then((category) => {
+        if (category !== undefined) {
+          res.render("admin/categories/edit", { category: category });
+        } else {
+          res.redirect("/admin/categories");
+        }
+      })
+      .catch((err) => {
+        console.log("Error while editing category: " + err);
+        res.redirect("/admin/categories");
+      });
+  } else {
+    res.redirect("/admin/categories");
+  }
+});
+
+router.post("/categories/update", (req, res) => {
+  const id = req.body.id;
+  const title = req.body.title;
+
+  Category.update(
+    { title: title, slug: slugify(title, { lower: true }) },
+    { where: { id: id } }
+  )
+    .then(() => {
+      res.redirect("/admin/categories");
+    })
+    .catch((err) => {
+      console.log("Error while updating category: " + err);
+      res.redirect("/admin/categories");
+    });
 });
 
 module.exports = router;
