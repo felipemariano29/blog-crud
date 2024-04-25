0fab28d0301670007441bb9e07cfb311a2c60236
Felipe Mariano <86112326+felipemariano29@users.noreply.github.com>
2024-04-25 14:42:55 -0300

    Created Article table with actions (create with TinyMCE and delete)

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,12 +1,72 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../categories/Category");
+const Article = require("./Article");
+const slugify = require("slugify");
 
-router.get("/articles", (req, res) => {
-  res.send("articles");
+router.get("/admin/articles", (req, res) => {
+  Article.findAll({
+    include: [{ model: Category }],
+  })
+    .then((articles) => {
+      res.render("admin/articles/index", { articles: articles });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/admin/articles");
+    });
 });
 
 router.get("/admin/articles/new", (req, res) => {
-  res.send("Create article");
+  Category.findAll()
+    .then((categories) => {
+      res.render("admin/articles/new", { categories: categories });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect("/admin/articles/");
+    });
+});
+
+router.post("/articles/save", (req, res) => {
+  const title = req.body.title;
+  const body = req.body.body;
+  const category = req.body.category;
+
+  Article.create({
+    title: title,
+    slug: slugify(title, { lower: true }),
+    body: body,
+    categoryId: category,
+  })
+    .then(() => {
+      res.redirect("/admin/articles");
+    })
+    .catch((err) => {
+      console.log("Error while saving article: " + err);
+      res.redirect("/admin/articles");
+    });
+});
+
+router.post("/articles/delete", (req, res) => {
+  const id = req.body.id;
+
+  if (id !== undefined && !isNaN(id)) {
+    Article.destroy({
+      where: {
+        id: id,
+      },
+    })
+      .then(() => {
+        res.redirect("/admin/articles");
+      })
+      .catch((err) => {
+        console.log("Error while deleting article: " + err);
+        res.redirect("/admin/articles");
+      });
+  } else {
+    res.redirect("/admin/articles");
+  }
 });
 
 module.exports = router;
--- database/database.js
+++ database/database.js
@@ -3,6 +3,7 @@ const sequelize = require("sequelize");
 const connection = new sequelize("blogcrud", "root", process.env.DB_PASSWORD, {
   host: "localhost",
   dialect: "mysql",
+  timezone: "-03:00",
 });
 
 module.exports = connection;
